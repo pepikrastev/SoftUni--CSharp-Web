427a81b56f3b29c529afb0293a1b83f19226e39e
the_freestyle <the_freestyle@abv.bg>
2020-02-02 21:33:16 +0200

    Homework

---
+++ C# Web Basics - January 2020/04. Workshop/SIS/DemoApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using SIS.HTTP;
+using SIS.HTTP.Response;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var routeTable = new List<Route>();
+            routeTable.Add(new Route(HttpMethodType.Get, "/", Index));
+            routeTable.Add(new Route(HttpMethodType.Get, "/users/login", Login));
+            routeTable.Add(new Route(HttpMethodType.Post, "/users/login", DoLogin));
+            routeTable.Add(new Route(HttpMethodType.Get, "/contact", Contact));
+            routeTable.Add(new Route(HttpMethodType.Get, "/favicon.ico", FavIcon));
+
+            var httpSurver = new HttpServer(80, routeTable);
+           await httpSurver.StartAsync();
+        }
+
+        private static HttpResponse FavIcon(HttpRequest request)
+        {
+            var byteContent = File.ReadAllBytes("wwwroot/favicon.ico");
+            return new FileResponse(byteContent, "image/x-icon");
+        }
+
+        private static HttpResponse Contact(HttpRequest request)
+        {
+            return new HtmlResponse("<h1>Contact</h1>");           
+        }
+
+        public static HttpResponse Index(HttpRequest request)
+        {
+            var username = request.SessionData.ContainsKey("Username") ? request.SessionData["Username"] : "Anonymous";
+            return new HtmlResponse($"<h1>Home page. Hello, {username}</h1>");
+        }
+
+        public static HttpResponse Login(HttpRequest request)
+        {
+            request.SessionData["Username"] = "Pesho";
+            return new HtmlResponse("<h1>login page</h1>");
+        }
+
+        public static HttpResponse DoLogin(HttpRequest request)
+        {
+            return new HtmlResponse("<h1>login page form</h1>");
+        }
+    }
+}
